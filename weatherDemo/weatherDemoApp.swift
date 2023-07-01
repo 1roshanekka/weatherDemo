77157fd931f6b39deba36894fe3d6358bbb0cf0b
Roshan Ekka <1ROSHANEKKA@GMAIL.COM>
2023-07-02 00:55:41 +0530

    Initial Commit

---
+++ weatherDemo/weatherDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  weatherDemoApp.swift
+//  weatherDemo
+//
+//  Created by Roshan Ekka on 02/07/23.
+//
+
+import SwiftUI
+
+@main
+struct weatherDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
